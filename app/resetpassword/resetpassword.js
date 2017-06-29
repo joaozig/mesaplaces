c9364c47d2e8c6e4e54b9fd1cdcdf1be25ef3775
João Ricardo Bastos <joaoricardobastos@gmail.com>
2017-06-29 19:43:10 -0300

    Add resetpassword via API

--- app/resetpassword/resetpassword.js
+++ app/resetpassword/resetpassword.js
@@ -4,10 +4,31 @@ angular.module('app.resetpassword', ['ui.router'])
 	$stateProvider.state('resetpassword', {
 		url: '/resetpassword',
 		templateUrl: 'resetpassword/resetpassword.html',
-		controller: 'ResetPasswordCtrl'
+		controller: 'ResetPasswordCtrl as ctrl'
 	});	
 }])
 
-.controller('ResetPasswordCtrl', [function() {
+.controller('ResetPasswordCtrl', ['$auth', '$location', function($auth, $location) {
+	var ctrl = this;
 
+	console.log(window.location.search.substr(1));
+
+	/** Properties **/
+	ctrl.isResetSuccess = false;
+	ctrl.isResetError = false;
+
+	/** Methods */
+	ctrl.resetPassword = function(user) {
+		$auth.requestPasswordReset(user)
+			.then(function(response) {
+				ctrl.isResetSuccess = true;
+				ctrl.isResetError = false;
+				console.log(response);
+			})
+			.catch(function(response) {
+				ctrl.isResetSuccess = false;
+				ctrl.isResetError = true;
+				console.log(response);
+			});
+	}
 }]);
