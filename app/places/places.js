5a0b2d1beb983b00dbf163f7dd79169b7be174bc
João Ricardo Bastos <joaoricardobastos@gmail.com>
2017-06-30 00:09:20 -0300

    Add places via API (javascript)

--- app/places/places.js
+++ app/places/places.js
@@ -4,49 +4,82 @@ angular.module('app.places', ['ui.router', 'ngGeolocation'])
 	$stateProvider.state('app.places', {
 		url: '/places',
 		templateUrl: 'places/places.html',
-		controller: 'PlacesCtrl'
+		controller: 'PlacesCtrl as ctrl'
 	});
 }])
 
-.controller('PlacesCtrl', ['$geolocation', '$scope', function($geolocation, $scope) {
+.controller('PlacesCtrl', ['$scope', '$geolocation', function($scope, $geolocation) {
+  var ctrl = this;
 
-	$scope.places = ['place 1', 'place 2', 'place 3'];
+  /** Attributes **/
+  ctrl.places;
+  ctrl.showLoading = true;
+  ctrl.showPlaces = false;
+  ctrl.showErrorMessage = false;
+  ctrl.errorMessage;
 
-	$geolocation.getCurrentPosition({
-		timeout: 60000
-	}).then(function(position) {
-		console.log('entrou')
-		console.log(position)
-		$scope.myPosition = position.coords.latitude + ' ' + position.coords.longitude;
-		initMap(position.coords.latitude, position.coords.longitude);
-	});
+  /** Methods **/
+  ctrl.init = init;
+
+  /** Initialization **/
+  ctrl.init();
+
+  /**********/
+  function init() {
+    ctrl.showErrorMessage = false;
+    ctrl.showLoading = true;
+
+    $geolocation.getCurrentPosition({
+      timeout: 15000
+    }).then(function(position) {
+      initMap(position.coords.latitude, position.coords.longitude);
+    },function(response) {
+      switch(response.error.code) {
+        case 1:
+          ctrl.errorMessage = 'Ative a sua geolocalização.'
+          break;
+        case 3:
+          ctrl.errorMessage = 'Tempo expirado.'
+          break;
+        default:
+          ctrl.errorMessage = 'Não foi possível obter os lugares.'
+      }
+      ctrl.showErrorMessage = true;
+      ctrl.showLoading = false;
+    });
+  }
 
   var map;
   var infowindow;
 
   function initMap(latitude, longitude) {
-    // var pyrmont = {lat: -33.867, lng: 151.195};
-    var pyrmont = {lat: latitude, lng: longitude};
+    ctrl.showPlaces = true;
+    ctrl.showLoading = false;
+    var centerLocal = {lat: latitude, lng: longitude};
 
     map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 15
+      center: centerLocal,
+      zoom: 16
     });
 
     infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
+    service = new google.maps.places.PlacesService(map);
     service.nearbySearch({
-      location: pyrmont,
-      radius: 500,
+      location: centerLocal,
+      radius: 300,
       type: ['store']
-    }, callback);
+    }, callbackPlaces);
   }
 
-  function callback(results, status) {
+  function callbackPlaces(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
+      ctrl.places = results;
+      $scope.$digest();
       for (var i = 0; i < results.length; i++) {
         createMarker(results[i]);
       }
+    } else {
+      alert('Não foi possível recuperar os lugares.');
     }
   }
 
